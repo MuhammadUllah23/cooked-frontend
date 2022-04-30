468043e19043cb3d77c51a600f72a477363e9a5c
muhammadullah <muhammadhullah23@gmail.com>
2022-04-30 17:30:13 -0400

    set up Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h2>Testing to see if this work</h2>
+      
+      <BrowserRouter >
+        <Routes>
+          <Route path="/" element={<div> Landing Page</div>}/>
+          <Route path="/login" element={<div> Login Page</div>}/>
+          <Route path="/inventory" element={<div>Inventory</div>}/>
+          <Route path="/history" element={<div>History</div>}/>
+        </Routes>
+      </BrowserRouter>
+        
+      
     </div>
   );
 }
