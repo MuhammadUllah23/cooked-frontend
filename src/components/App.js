0745ec6c7d8a3d075597b78d29c17e245789c130
muhammadullah <muhammadhullah23@gmail.com>
2022-05-01 15:18:09 -0400

    import ThemeProvider and wrap it around BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,23 @@
-
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { ThemeProvider } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      
-      <BrowserRouter >
-        <Routes>
-          
-          <Route path="/" element={<div> Landing Page</div>}/>
-          <Route path="/login" element={<div> Login Page</div>}/>
-          {/* TODO: set up routes below to only render page if user is logged in. If not then navigate to login page.*/}
-          <Route path="/inventory" element={<div>Inventory</div>}/>
-          <Route path="/history" element={<div>History</div>}/>
-        </Routes>
-      </BrowserRouter>
-        
+      <ThemeProvider>
+        <BrowserRouter >
+          <Routes>
+            
+            <Route path="/" element={<div> Landing Page</div>}/>
+            <Route path="/login" element={<div> Login Page</div>}/>
+            {/* TODO: set up routes below to only render page if user is logged in. If not then navigate to login page.*/}
+            <Route path="/inventory" element={<div>Inventory</div>}/>
+            <Route path="/history" element={<div>History</div>}/>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
       
     </div>
   );
