c68b719f486032c7bd270f3269494567d9ee6afa
muhammadullah <muhammadhullah23@gmail.com>
2022-05-01 15:49:23 -0400

    set primary palette color to matisse and secondary to darkOrchid

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,5 +1,15 @@
 import { createTheme } from "@mui/material";
 
-const theme = createTheme({
-    
+const matisse = "#355C7D"
+const darkOrchid = "#6A1F9A"
+
+export default createTheme({
+    palette: {
+        primary: {
+            main: matisse
+        },
+        secondary: {
+            main: darkOrchid
+        }
+    }
 })
