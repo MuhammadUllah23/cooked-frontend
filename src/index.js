de8268c0d0753eb837d0bd699e593dcdca062013
muhammadullah <muhammadhullah23@gmail.com>
2022-05-04 02:16:08 -0400

    import redux/toolkit, and react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Provider } from 'react-redux'
-import { configureStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk'
 
 import rootReducer from './components/reducers/rootReducer'
