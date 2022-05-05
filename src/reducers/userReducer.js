24356a528e68d3821103870343943c4d19e4bbac
muhammadullah <muhammadhullah23@gmail.com>
2022-05-05 11:20:22 -0400

    create userReducer

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,11 @@
+import { SET_USER } from "../actions/constants";
+
+export default function userReducer(state= [], action){
+    switch (action.type) {
+        case SET_USER:
+            return action.payload
+
+        default:
+            return state
+    }
+}
