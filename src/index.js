c07610b122ca91365c2e82a6cfd32076b7af84ae
muhammadullah <muhammadhullah23@gmail.com>
2022-05-16 11:24:27 -0400

    fix rootReducer import error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk'
 
-import rootReducer from './reducers/rootReducer'
+import { rootReducer } from './reducers/rootReducer';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
