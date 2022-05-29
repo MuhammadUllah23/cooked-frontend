c85a252411be572167d8effd2e85cddfdfccc2a6
muhammadullah <muhammadhullah23@gmail.com>
2022-05-29 17:58:15 -0400

    ReactDOM is being imported from react
    ReactDOM needs to be imoprted from react-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom';
+import { ReactDOM } from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 
@@ -22,7 +22,7 @@ ReactDOM.render(
   <React.StrictMode>
   <Provider store={store} >
     <BrowserRouter>
-      <div className="wrapper" >
+      <div className="wrapper">
       <App />
       </div>
     </BrowserRouter>
