a6dfc07b27ff9bf31397559a4b2a5fb73af54cd9
muhammadullah <muhammadhullah23@gmail.com>
2022-06-07 01:41:49 -0400

    change title to Cooked

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ ReactDOM.render(
   <Provider store={store} >
     <BrowserRouter>
       <div className="wrapper">
-      {/* <App /> */}
+      <App />
       </div>
     </BrowserRouter>
   </Provider>
