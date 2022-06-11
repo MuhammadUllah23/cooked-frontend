6bf47d493172bc9fc27bbd46683b4a1886cef804
muhammadullah <muhammadhullah23@gmail.com>
2022-06-10 22:26:57 -0400

    them is not working in makeStyles

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,30 @@
 import * as React from "react";
 
-import { Grid, useMediaQuery } from "@mui/material";
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Button } from "@mui/material";
 import { TextField } from "@mui/material";
+import Theme from "./ui/Theme";
+import { makeStyles } from "@mui/material";
+
+const useStyles = makeStyles((theme) => ({
+    canvas: {
+        backgroundColor: theme
+    }
+}))
 
 export default function Login() {
+    const classes = useStyles()
+    const theme = useTheme()
 
     return (
+        
         // Simple Login and sign up page.
         // Card in the center of Window with tabs on top to switch between login form and signup form
         // colors: Dark Orchid as backgorund color
         
+        <Grid container className={classes.canvas} >
+
+        </Grid>
     )
 }
