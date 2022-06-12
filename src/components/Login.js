c755cd6e8d692e4f69837ab329068856cbf9ed98
muhammadullah <muhammadhullah23@gmail.com>
2022-06-12 16:03:38 -0400

    replace div with box. Use same styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,26 @@
 import * as React from "react";
 
-import { Grid, useMediaQuery, useTheme } from "@mui/material";
+import { Grid, Box, useMediaQuery, useTheme } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Button } from "@mui/material";
 import { TextField } from "@mui/material";
 import Theme from "./ui/Theme";
-import styled from "@mui/material";
+// import styled from "@mui/material";
 
 import './Login.css'
 
 
-
+// const styledBox = styled(Box)({
+//     margin: "auto",
+    
+//     width: "70%",
+//     height: "80vh",
+//     border: "2px solid green",
+    
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContent: "center",
+// })
 
 
 export default function Login() {
@@ -23,16 +33,23 @@ export default function Login() {
         // Card in the center of Window with tabs on top to switch between login form and signup form
         // colors: Dark Orchid as backgorund color
         <div className="canvas">
-           <div id="loginBox">
+           {/* <styledBox> */}
+           <Box sx={{
+                margin: "auto", width: "70%", height: "80vh", border: "2px solid green",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+                }}>
                 <Grid container sx={{height: "100%", justifyContent: "space-between", backgroundColor: "white",}}>
-                <Grid item sx={{backgroundColor: "black", height: "100%", width: "50%"}}>
-                    <h1>hello</h1>
+                    <Grid item sx={{backgroundColor: "black", height: "100%", width: "50%"}}>
+                        <h1>hello</h1>
+                    </Grid>
+                    <Grid item>
+                        <h1>hello</h1>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <h1>hello</h1>
-                </Grid>
-                </Grid>
-           </div>
+            </Box>   
+           {/* </styledBox> */}
         </div>
     )
 }
