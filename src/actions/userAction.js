d89fca4c803b00e9263ceb50cbaae0b4df335c78
muhammadullah <muhammadhullah23@gmail.com>
2022-07-02 15:17:27 -0400

    create loginUser Action

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,5 +1,24 @@
 import { GET_USER, SET_PRODUCTS } from "./constants";
 
 export function loginUser(user, navigate) {
-    
+    return dispatch => {
+        fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({user}),
+        })
+            .then((res) => {
+                if (res.ok) {
+                    localStorage.setItem("token", res.headers.get("Authorization"));
+                    return res.json()
+                } 
+            })
+            .then((user) => {
+                dispatch({type: GET_USER, payload: user.data})
+                dispatch({type: SET_PRODUCTS, payload: user.data.products})
+                // navigate to user profile
+            })
+    }
 }
