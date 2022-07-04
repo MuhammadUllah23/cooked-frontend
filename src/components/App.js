861120e6755d12147bb78a3fdc0a8025e824b66a
muhammadullah <muhammadhullah23@gmail.com>
2022-07-03 22:23:25 -0400

    test navigating

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import theme from './ui/Theme'
 
@@ -8,11 +9,13 @@ import { Button, ThemeProvider } from '@mui/material';
 import Login from './Login';
 
 function App() {
+  const navigate = useNavigate()
+
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
           <Routes>
-            <Route path="/" element={<div>Landing Page. Back to main branch</div>}/>
+            <Route path="/" element={<div>Landing Page. Back to main branch <button onClick={() => navigate('/login')}>Login Page</button></div>}/>
             <Route path="/login" element={<Login />}/>
             {/* TODO: set up routes below to only render page if user is logged in. If not then navigate to login page.*/}
             <Route path="/inventory" element={<div>Inventory</div>}/>
