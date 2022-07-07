5a3bc2c19e9d948ed1755c9c48860455a39ce707
muhammadullah <muhammadhullah23@gmail.com>
2022-07-07 02:51:56 -0400

    Create To Do list for Inventory page

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,7 +2,16 @@ import * as React from "react";
 
 export default function Inventory() {
 
+// TODO: Set up blueprint for Grid system
+// TODO: Come up with function that calculates acccumulated profit of all inventory sold minus the inventory on hand that will be showcased at the top
+// TODO: Create add Product button that opens a dialogue of a form to add a new product to your inventory.
+// TODO: Each Product should be showcased in a bar with how much the item was bought for, how much the item sold for and and profit margin. 
+// TODO: Product Tab should have an edit button that opens up a dialogue with an edit form.
+// TODO: Product Tab should have a 'show more info' button that opens up a dialogue that gives all the details of the product.
+// TODO: Product Tab should have a delete button. When clicked it shold open a 'Are You Sure?' Box with a Yes and No Button.  
+
     return (
-        <h1>Inventory Page</h1>
+        <div id="inventoryPage"> 
+        </div>
     )
 }
