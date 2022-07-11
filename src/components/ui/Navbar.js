45da87a9d3410bc987d2785c50f71516664d226e
muhammadullah <muhammadhullah23@gmail.com>
2022-07-11 02:41:36 -0400

    create todo list for navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 export default function Navbar() {
+// This will be the navigation bar and header that will only render for the logged in user to navigate between the tabs.
+// The nav bar is to the left of the screen with the tabs 'Dashboard', 'Inventory', and 'History'. May add more to the future.
+// There will also be a simple header with the name of the web site, the user's username, and a logout button.
+
+// TODO: Set up the box that will fill up the screen about 90%-95%.
+// ? Refer to the Arc Development project to build the side nav bar and the header.
+
     return (
         <React.Fragment>
             <h1>this will be the navigation.</h1>
