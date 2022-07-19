52a827f5f347abbf3e38a8f995851df9d258ce2d
muhammadullah <muhammadhullah23@gmail.com>
2022-07-19 02:12:21 -0400

    styled components should be set outside function

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,12 +7,13 @@ import { useNavigate } from "react-router-dom";
 import { styled } from '@mui/material/styles';
 
 
+const CustomButton = styled(Button)(({ theme }) => ({
+        color: theme.palette.secondary.dark
+    }))
 export default function LandingPage() {
     const navigate = useNavigate()
 
-    const CustomButton = styled(Button)(({ theme }) => ({
-        color: theme.palette.secondary.dark
-    }))
+    
 
     return (
         <div>Landing Page. Back to main branch <CustomButton onClick={() => navigate('/login')}>Login Page</CustomButton></div>
