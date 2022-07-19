4fbba5ab9402922451efefc7f665fdf3241d4635
muhammadullah <muhammadhullah23@gmail.com>
2022-07-19 02:09:19 -0400

    create elevationScrollTrigger and call it in Navbar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -16,7 +16,23 @@ import AppBar from "@mui/material";
 import Menu from "@mui/material";
 import MenuItem from "@mui/material";
 import SwipeableDrawer from "@mui/material";
-import useScrollTrigger from "@mui/material";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+
+
+function ElevationScroll(props) {
+    const { children } = props;
+    // useScrollTrigger is an event listener
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,   
+      threshold: 0
+    //   how far a user needs to scroll to trigger event listener. Default is 100.
+    });
+    
+    // returns new version of component that is being wrapped by ElevationScroll. Clones the children and returns a new copy of the elevation when triggered
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+}
 
 
 export default function Navbar(props) {
@@ -55,7 +71,9 @@ const handleChange = (e, newValue) => {
 
     return (
         <React.Fragment>
-            {tabs}
+           <ElevationScroll>
+            
+           </ElevationScroll>
         </React.Fragment>
     )
 }
