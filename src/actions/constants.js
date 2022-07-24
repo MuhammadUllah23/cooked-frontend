e0c9e39942a6ebbf01ba54a3c52031b8aad45611
muhammadullah <muhammadhullah23@gmail.com>
2022-07-24 19:19:29 -0400

    LOGOUT_USER constant

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,5 +1,6 @@
 export const SET_USER = "SET_USER"
-export const GET_USER = "GET_USER"
+export const LOGOUT_USER = "LOGOUT_USER"
+
 
 export const SET_PRODUCTS = "SET_PRODUCTS"
 export const ADD_PRODUCT = "ADD_PRODUCT"
