cc18b01a2832fcd5b3e3969a5ccb5108473a73b5
muhammadullah <muhammadhullah23@gmail.com>
2022-07-24 19:20:40 -0400

    User reducer handles logout by returning empty array

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,9 +1,12 @@
-import { SET_USER } from "../actions/constants";
+import { SET_USER, LOGOUT_USER } from "../actions/constants";
 
 export default function userReducer(state= [], action){
     switch (action.type) {
         case SET_USER:
             return action.payload
+        
+        case LOGOUT_USER:
+            return []
 
         default:
             return state
